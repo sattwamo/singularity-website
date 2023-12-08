772b391a99e19d69a54719212b76f245a0efc62b
Sattwamo Ghosh <sattwamoghosh@gmail.com>
2023-12-08 20:30:06 +0530

    text update

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -6,7 +6,7 @@
     <section id="introPage" class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14">
         <div class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10">
             <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
-                Hi! <span class="poppins text-violet-400">Text 1</span> Text 2
+                Text goes <span class="poppins text-violet-400">Here</span>
             </h2>
         </div>
 

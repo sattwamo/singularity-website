431facf9823b7c303f2136b1466401d85a92c7e5
Sattwamo Ghosh <sattwamoghosh@gmail.com>
2023-12-08 20:25:18 +0530

    v0

---
+++ src/components/Main.svelte
@@ -0,0 +1,14 @@
+<script>
+
+</script>
+
+<main class="flex flex-col flex-1 p-4">
+    <section id="introPage" class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14">
+        <div class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10">
+            <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
+                Hi! <span class="poppins text-violet-400">Text 1</span> Text 2
+            </h2>
+        </div>
+
+    </section>
+</main>

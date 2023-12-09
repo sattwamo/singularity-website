290e9bea82fa7f73b602422395887a225c10b202
Sattwamo Ghosh <sattwamoghosh@gmail.com>
2023-12-09 18:49:40 +0530

    added scroll

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,4 +1,4 @@
-<footer class="py-20 sm:py-32 bg-black border-t border-solid border-violet-950 flex flex-col gap-4 sm:gap-8 justify-center items-center">
+<footer class="py-20 sm:py-32 bg-black border-t border-solid border-violet-950 flex flex-col gap-4 sm:gap-8 justify-center items-center" id="contact">
     <p class="px-4 py-2 bg-white text-slate-950 font-medium">
         Get in touch &darr;
     </p>
